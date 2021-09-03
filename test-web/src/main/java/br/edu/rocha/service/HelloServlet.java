bdfa2bef72202d839278b0a4b6ed8ab6f0b207af
Roberto Rocha <rrocha.roberto@gmail.com>
2021-09-02 21:36:17 -0300

    Fix servlet URL.

--- test-web/src/main/java/br/edu/rocha/service/HelloServlet.java
+++ test-web/src/main/java/br/edu/rocha/service/HelloServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("HelloServlet")
+@WebServlet("/HelloServlet")
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
